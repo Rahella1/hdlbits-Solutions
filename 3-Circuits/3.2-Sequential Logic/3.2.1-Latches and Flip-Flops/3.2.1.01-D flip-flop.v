e3e72703f105eb24f7515539a0d59b164f696689
Rahel Gebremedhin <lehera16@gmail.com>
2026-08-27 17:07:07 -0500

    Flip Flop

---
+++ 3-Circuits/3.2-Sequential Logic/3.2.1-Latches and Flip-Flops/3.2.1.01-D flip-flop.v
@@ -0,0 +1,10 @@
+module top_module (
+    input clk,    // Clocks are used in sequential circuits
+    input d,
+    output reg q );//
+
+    //non-blocking assignment
+    always @(posedge clk)begin
+        q <=d;
+    end
+endmodule
